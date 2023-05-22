29c1aeccc5ffffbd1ce806c0442ed423319af1eb
MSBivens <mikebiv95@gmail.com>
2023-05-21 19:54:12 -0700

    Wrapper for navigation and socials

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BsTwitter, BsGithub, BsMedium } from "react-icons/bs";
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsTwitter />
+    </div>
+    <div>
+      <BsGithub />
+    </div>
+    <div>
+      <BsMedium />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
---
+++ frontend_react/src/wrapper/AppWrapp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavigationDots, SocialMedia } from "../components";
+
+const AppWrap = (Component, idName, classNames) =>
+  function HOC() {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+
+        <NavigationDots active={idName} />
+      </div>
+    );
+  };
+
+export default AppWrap;
