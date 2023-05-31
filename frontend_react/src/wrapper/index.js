8410dc40a8ca80b259d8a11bdb1a2650dc684f1f
MSBivens <mikebiv95@gmail.com>
2023-05-30 19:05:28 -0700

    cleanup and error fixing

--- frontend_react/src/wrapper/index.js
+++ frontend_react/src/wrapper/index.js
@@ -1,5 +1,4 @@
-import NavigationDots from "./NavigationDots";
-import SocialMedia from "./SocialMedia";
-import Navbar from "./Navbar/Navbar";
+import AppWrap from "./AppWrap";
+import MotionWrap from "./MotionWrap";
 
-export { NavigationDots, SocialMedia, Navbar };
+export { AppWrap, MotionWrap };
